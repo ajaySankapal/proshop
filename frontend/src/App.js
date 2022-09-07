1815e19352300ec803278caf8dff34a9ed0afbd1
ajaySankapal <90617524+ajaySankapal@users.noreply.github.com>
2022-09-07 15:17:44 +0530

    front end and started backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,25 @@
-function App() {
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+
+const App = () => {
   return (
-    <div>
-      <h2>ProShop</h2>
-    </div>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Routes>
+          {/* <Container> */}
+          <Route path='/' element={<HomeScreen />} exact />
+          {/* </Container> */}
+          <Route path='/product/:id' element={<ProductScreen />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
   )
 }
 
