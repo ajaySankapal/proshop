be533b969da5fbefaa90c678345cab550b42c285
ajaySankapal <90617524+ajaySankapal@users.noreply.github.com>
2022-09-15 23:25:33 +0530

    introduction and implementation of reduc

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 
+// to connect front to backend we have to set the proxy, because for front end there is diff url and for backend there is different url.. so we set the url of the backend from which we want to access the data in proxy ,,, for local host "proxy":"http://127.0.0.1:5000",
 const App = () => {
   return (
     <Router>
